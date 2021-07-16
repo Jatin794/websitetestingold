0ddfd7497eeb191f8f77d96720bf6fa3e58adf2b
Jatin <jatin.nagpal222@gmail.com>
2021-07-17 02:39:25 +0530

    retry

---
+++ .cache/sync-requires.js
@@ -0,0 +1,15 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/home/jatin/Documents/Github/websitetesting/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/jatin/Documents/Github/websitetesting/src/pages/404.js")),
+  "component---src-templates-author-js": preferDefault(require("/home/jatin/Documents/Github/websitetesting/src/templates/author.js")),
+  "component---src-templates-index-js": preferDefault(require("/home/jatin/Documents/Github/websitetesting/src/templates/index.js")),
+  "component---src-templates-page-js": preferDefault(require("/home/jatin/Documents/Github/websitetesting/src/templates/page.js")),
+  "component---src-templates-post-js": preferDefault(require("/home/jatin/Documents/Github/websitetesting/src/templates/post.js")),
+  "component---src-templates-tag-js": preferDefault(require("/home/jatin/Documents/Github/websitetesting/src/templates/tag.js"))
+}
+
